8d0a2d3ba70babdef6504f879dabbf4d143a00e6
bhighlander <saphrimangel@gmail.com>
2023-03-10 18:13:51 -0600

    cleaned up typos and added readme.md

--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,7 +1,6 @@
 const clearDom = () => {
   document.querySelector('#cards').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
-  document.querySelector('#filter-buttons').innerHTML = '';
   document.querySelector('#cards').innerHTML = '';
 };
 
