14db25e6d17afa5cf2c4203fc3f947aed1b12053
bhighlander <saphrimangel@gmail.com>
2023-03-11 08:47:51 -0600

    FIX: fixed filter button not clearing dom

--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,5 +1,6 @@
 const clearDom = () => {
   document.querySelector('#cards').innerHTML = '';
+  document.querySelector('#filter-buttons').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
   document.querySelector('#cards').innerHTML = '';
 };
